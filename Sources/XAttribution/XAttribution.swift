23b8d6383ab78e630b65ff472e9927cd2123c09b
awxkee <radzivon.bartoshyk@proton.me>
2024-03-26 11:20:21 +0000

    improved collection

--- Sources/XAttribution/XAttribution.swift
+++ Sources/XAttribution/XAttribution.swift
@@ -1,6 +1,6 @@
 //
 //  XAttrubution.swift
-//  
+//
 //
 //  Created by Radzivon Bartoshyk on 28/03/2023.
 //
@@ -11,13 +11,13 @@ import UIKit
 import AppTrackingTransparency
 
 public final class XAttrubution {
-
+    
     private let key: String
     private let url: String
     private let session: URLSession
     private let userDefaults: UserDefaults
     private var userId: String?
-
+    
     init(key: String, url: String, session: URLSession, userDefaults: UserDefaults, userId: String?) {
         self.key = key
         self.url = url
@@ -25,7 +25,7 @@ public final class XAttrubution {
         self.userDefaults = userDefaults
         self.userId = userId
     }
-
+    
     public static func instance(key: String,
                                 url: String = "https://hq.cacaomeasure.com",
                                 session: URLSession = .shared,
@@ -37,15 +37,15 @@ public final class XAttrubution {
                             userDefaults: userDefaults,
                             userId: userId)
     }
-
+    
     public func set(userId: String?) {
         self.userId = userId
     }
-
+    
     public func reset() {
         userDefaults.set(nil, forKey: "__aaa_x_attrubution_sent")
     }
-
+    
     public func collect() async throws -> [String: Any] {
         try await withCheckedThrowingContinuation { cont in
             collect { rs, err in
@@ -59,7 +59,7 @@ public final class XAttrubution {
             }
         }
     }
-
+    
     public func collect(_ completion: (([String: Any]?, Error?) -> ())? = nil) {
         if let value = userDefaults.object(forKey: "__aaa_x_attrubution_sent") as? Bool, value {
             DispatchQueue.main.async {
@@ -70,7 +70,7 @@ public final class XAttrubution {
         if #available(iOS 14.3, *) {
             Task {
                 do {
-                    guard var attribution = await getAttribution() else {
+                    guard var attribution = try await getAttribution() else {
                         await MainActor.run { completion?(nil, XAttributionCollectionError()) }
                         return
                     }
@@ -78,15 +78,15 @@ public final class XAttrubution {
                     attribution.removeValue(forKey: "attribution")
                     let systemVersion = await MainActor.run { UIDevice.current.systemVersion }
                     attribution["ios_version"] = systemVersion
-
+                    
                     if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
                         attribution["app_version"] = appVersion
                     }
-
-                    attribution["sdk_version"] = "1.0.4"
-
+                    
+                    attribution["sdk_version"] = "2.0.1"
+                    
                     let attStatus = await MainActor.run { ATTrackingManager.trackingAuthorizationStatus }
-
+                    
                     switch attStatus {
                     case .notDetermined:
                         attribution["att_status"] = "notDetermined"
@@ -99,11 +99,11 @@ public final class XAttrubution {
                     @unknown default:
                         break
                     }
-
+                    
                     if let userId {
                         attribution["user_id"] = userId
                     }
-
+                    
                     let deviceType = await MainActor.run { UIDevice.current.userInterfaceIdiom }
                     if deviceType == .phone {
                         attribution["device_type"] = "phone"
@@ -112,16 +112,16 @@ public final class XAttrubution {
                     } else {
                         attribution["device_type"] = "other"
                     }
-
+                    
                     attribution["idfa"] = await MainActor.run { ASIdentifierManager.shared().advertisingIdentifier.uuidString }
-
+                    
                     let bundle = Bundle.main.bundleIdentifier
                     if let bundle {
                         attribution["bundle"] = bundle
                     }
-
+                    
                     attribution["key"] = key
-
+                    
                     guard var components = URLComponents(string: self.url) else {
                         await MainActor.run { completion?(nil, XAttributionCollectionError()) }
                         return
@@ -138,12 +138,12 @@ public final class XAttrubution {
                     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
                     request.addValue("application/json", forHTTPHeaderField: "Accept")
                     request.httpBody = try JSONSerialization.data(withJSONObject: attribution, options: .prettyPrinted)
-
+                    
                     let (_, response) = try await self.session.data(for: request)
                     
                     await MainActor.run {
                         if let urlResponse = (response as? HTTPURLResponse), 200..<300 ~= urlResponse.statusCode {
-                            userDefaults.set(true, forKey: "__aaa_x_attrubution_sent")
+                            finalizeCollection()
                             completion?(fMethod, nil)
                         } else {
                             completion?(nil, XAttributionCollectionError())
@@ -159,62 +159,88 @@ public final class XAttrubution {
             }
         }
     }
-
+    
+    private func finalizeCollection() {
+        userDefaults.set(true, forKey: "__aaa_x_attrubution_sent")
+    }
+    
     @available(iOS 14.3, *)
-    private func getAttribution() async -> [String: Any]? {
+    private func getAttribution() async throws -> [String: Any]? {
+        /**
+         https://developer.apple.com/documentation/adservices/aaattribution/attributiontoken()#Attribution-payload
+         */
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "api-adservices.apple.com"
+        urlComponents.path = "/api/v1/"
+        let body: Data
         do {
-            var urlComponents = URLComponents()
-            urlComponents.scheme = "https"
-            urlComponents.host = "api-adservices.apple.com"
-            urlComponents.path = "/api/v1/"
             let token = try AAAttribution.attributionToken()
-            let body = Data(token.utf8)
-            
-            guard let url = urlComponents.url else {
-                return nil
+            body = Data(token.utf8)
+        } catch {
+            // finalize collection no sense to proceed
+            self.finalizeCollection()
+            throw XAttributionTokenGenerationError(underlying: error)
+        }
+        
+        guard let url = urlComponents.url else {
+            throw XAttributionInvalidStateError(message: "Cannot construct valid URL")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body
+        
+        var (data, response) = try await session.data(for: request)
+        guard let urlResponse = (response as? HTTPURLResponse) else {
+            throw XAttributionInvalidStateError(message: "Cannot construct valid response from URL request")
+        }
+        var statusCode = urlResponse.statusCode
+        var retries: Int = 0
+        if statusCode == 404 {
+            /**
+             404 signals an
+             Not found. The API is unable to retrieve the requested attribution record.Tokens have a TTL of 24 hours. If the POST API call exceeds 24 hours, a 404 response returns. If your token is valid, a best practice is to initiate retries at intervals of 5 seconds with a maximum of three attempts.
+             */
+            // Do 3 retries with delay.
+            while retries < 3 && statusCode == 404 {
+                try await Task.sleep(nanoseconds: NSEC_PER_SEC*4)
+                (data, response) = try await session.data(for: request)
+                guard let urlResponse = (response as? HTTPURLResponse) else {
+                    throw XAttributionInvalidStateError(message: "Cannot construct valid response from URL request")
+                }
+                statusCode = urlResponse.statusCode
+                retries += 1
             }
-            
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.addValue("text/plain", forHTTPHeaderField: "Content-Type")
-            request.httpBody = body
-            
-            let (data, response) = try await session.data(for: request)
-            guard 
-                let urlResponse = (response as? HTTPURLResponse), 200..<300 ~= urlResponse.statusCode,
-                let attribution = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
-            else {
-                return nil
-            }
-                        
+        }
+        
+        /**
+         400 - The token is invalid.
+         */
+        if statusCode == 400 {
+            // finalize collection no sense to proceed
+            self.finalizeCollection()
+            throw XAttributionInvalidTokenConditionError()
+        }
+        
+        if 200..<300 ~= statusCode, let attribution = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
             var att: [String: Any] = [:]
             attribution.forEach { key, value in att[key] = value }
             if attribution["attribution"] as? Bool == true {
                 return attribution
+            } else {
+                throw XAttributionEmptyCollectionError()
             }
-            return nil
-        } catch {
-            return nil
         }
-    }
-
-    public struct XAttributionAlreadyCollectedError: LocalizedError, CustomNSError {
-        public var errorDescription: String {
-            "Attribution already collected"
-        }
-
-        public var errorUserInfo: [String : Any] {
-            [NSLocalizedDescriptionKey: errorDescription]
-        }
-    }
-
-    public struct XAttributionCollectionError: LocalizedError, CustomNSError {
-        public var errorDescription: String {
-            "Attribution cannot be collected"
-        }
-
-        public var errorUserInfo: [String : Any] {
-            [NSLocalizedDescriptionKey: errorDescription]
-        }
+        /**
+         
+         500 - The Apple Search Ads server is temporarily down or unreachable. The request may be valid, but you need to retry it later.
+         */
+        /**
+         Skip for now allow -> allow retry later
+         */
+        
+        return nil
     }
 }
